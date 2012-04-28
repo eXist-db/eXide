57cace480948d3a03b6a1d2e420e72f730432c89
Wolfgang Meier <wolfgangmm@gmail.com>
2012-04-28 13:21:27 +0200

    Dropdown menus should stay open until user clicks elsewhere.

--- src/menu.js
+++ src/menu.js
@@ -24,24 +24,32 @@ eXide.namespace("eXide.util.Menubar");
 eXide.util.Menubar = (function() {
     
     Constr = function (container) {
+        var $this = this;
         this.container = container;
         
+        var menuVisible = false;
+        
         // Display sub menu on click
-		$("ul li", this.container).click(function () {
+		$("ul li", this.container).click(function (ev) {
+            ev.stopPropagation();
             var $this = this;
-            $("ul", this).css({visibility: "visible", display: "none"}).show(400);
-            $(this).mouseleave(function() {
-                $("ul", $this).css({visibility: "hidden"});
-            });
+            $("ul", this).css({display: "block"});
+            menuVisible = true;
 		});
+        $("body").click(function () {
+            if (menuVisible) {
+                $("ul li ul", $this.container).css({display: "none"});
+            }
+        });
     };
     
     Constr.prototype.click = function(selector, callback) {
         var $this = this;
         $(selector).click(function(ev) {
             ev.preventDefault();
+            $("ul li ul", $this.container).css({display: "none"});
+            menuVisible = false;
             callback();
-            $("ul", $this).css({visibility: "hidden"});
         });
     };
     
