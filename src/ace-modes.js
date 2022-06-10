51dbe21851912cfe8108788ec60897359cc95fca
marmoure <tinky93winky@gmail.com>
2022-06-10 12:15:54 +0200

    minimal solution to make autopair optional

--- src/ace-modes.js
+++ src/ace-modes.js
@@ -148,7 +148,7 @@ define("eXide/mode/xquery_highlight_rules", function(require, exports, module) {
         var oop = require("ace/lib/oop");
         var Behaviour = require('ace/mode/behaviour').Behaviour;
         var CstyleBehaviour = require('ace/mode/behaviour/cstyle').CstyleBehaviour;
-
+    
         var context;
         var contextCache = {};
         var defaultQuotes = {'"' : '"', "'" : "'"};
@@ -187,9 +187,6 @@ define("eXide/mode/xquery_highlight_rules", function(require, exports, module) {
         };
     
         var XQueryBehaviour = function (parent) {
-            
-            this.inherit(CstyleBehaviour, ["braces", "parens", "string_dquotes"]); // Get string behaviour
-            this.parent = parent;
             
             this.add("brackets", "insertion", function (state, action, editor, session, text) {
                 if (text == "\n") {
@@ -231,13 +228,18 @@ define("eXide/mode/xquery_highlight_rules", function(require, exports, module) {
                             text: '\n' + " : ",
                             selection: [1, 3, 1, 3]
                         }
+                    }else if (line.match(/^\s:/) && line.match(/^\s:/) && cursor.column < line.length) {
+                        return {
+                          text: "\n : ",
+                          selection: [1, 3, 1, 3]
+                        };
                     }
                 }
                 if (text == ":") {
                     if (line.slice(cursor.column -1 ,cursor.column) === '(') {
                         return {
                             text: ":  :",
-                            selection: [2, 2]
+                            selection: []
                         }
                     }
                 }
@@ -259,108 +261,21 @@ define("eXide/mode/xquery_highlight_rules", function(require, exports, module) {
                 return false;
             });
     
-            this.add("string_dquotes", "insertion", function(state, action, editor, session, text) {
-                var quotes = defaultQuotes;
+            this.add("autoPairDisabled", "insertion", function(state, action, editor, session, text) {
                 var disablePair = eXide.app.getEditor().getActiveDocument().disableAutoPair
-                if (text.length == 1 && quotes[text]) {
-                    if (this.lineCommentStart && this.lineCommentStart.indexOf(text) != -1) 
-                        return;
-                    initContext(editor);
-                    var quote = text;
-                    var selection = editor.getSelectionRange();
-                    var selected = session.doc.getTextRange(selection);
-                    if (selected !== "" && (selected.length != 1 || !quotes[selected]) && editor.getWrapBehavioursEnabled()) {
-                        return getWrapped(selection, selected, quote, quote);
-                    } else if (!selected) {
-                        var cursor = editor.getCursorPosition();
-                        var line = session.doc.getLine(cursor.row);
-                        var leftChar = line.substring(cursor.column-1, cursor.column);
-                        var rightChar = line.substring(cursor.column, cursor.column + 1);
-                        
-                        var token = session.getTokenAt(cursor.row, cursor.column);
-                        var rightToken = session.getTokenAt(cursor.row, cursor.column + 1);
-                        // We're escaped.
-                        if (leftChar == "\\" && token && /escape/.test(token.type))
-                            return null;
-                        
-                        var stringBefore = token && /string|escape/.test(token.type);
-                        var stringAfter = !rightToken || /string|escape/.test(rightToken.type);
-                        
-                        var pair;
-                        if (rightChar == quote) {
-                            pair = stringBefore !== stringAfter;
-                            if (pair && /string\.end/.test(rightToken.type))
-                                pair = false;
-                        } else {
-                            if (stringBefore && !stringAfter)
-                                return null; // wrap string with different quote
-                            if (stringBefore && stringAfter)
-                                return null; // do not pair quotes inside strings
-                            var wordRe = session.$mode.tokenRe;
-                            wordRe.lastIndex = 0;
-                            var isWordBefore = wordRe.test(leftChar);
-                            wordRe.lastIndex = 0;
-                            var isWordAfter = wordRe.test(leftChar);
-                            if (isWordBefore || isWordAfter)
-                                return null; // before or after alphanumeric
-                            if (rightChar && !/[\s;,.})\]\\]/.test(rightChar))
-                                return null; // there is rightChar and it isn't closing
-                            var charBefore = line[cursor.column - 2];
-                            if (leftChar == quote &&  (charBefore == quote || wordRe.test(charBefore)))
-                                return null;
-                            pair = true;
-                        }
-                        if(!disablePair) {
-                            return {
-                                text: pair ? quote + quote : "",
-                                selection: [1,1]
-                            };
-                        }else return null
-                    }
+                if(!disablePair) return null;
+                if (['(',"\"","'","{"].includes(text)) {
+                    return {
+                        text,
+                        selection: [1, 1]
+                    };
                 }
             });
             
-            this.add("parens", "insertion", function(state, action, editor, session, text) {
-                if (text == '(') {
-                    initContext(editor);
-                    var disablePair = eXide.app.getEditor().getActiveDocument().disableAutoPair
-                    var selection = editor.getSelectionRange();
-                    var selected = session.doc.getTextRange(selection);
-                    if (selected !== "" && editor.getWrapBehavioursEnabled()) {
-                        return getWrapped(selection, selected, '(', ')');
-                    } else if (CstyleBehaviour.isSaneInsertion(editor, session)) {
-                        if(!disablePair) {
-                            CstyleBehaviour.recordAutoInsert(editor, session, ")");
-                            return {
-                                text: '()',
-                                selection: [1, 1]
-                            };
-                        }else {
-                            return {
-                                text: '(',
-                                selection: [1, 1]
-                            };
-                        }
-                    }
-                } else if (text == ')') {
-                    initContext(editor);
-                    var cursor = editor.getCursorPosition();
-                    var line = session.doc.getLine(cursor.row);
-                    var rightChar = line.substring(cursor.column, cursor.column + 1);
-                    if (rightChar == ')') {
-                        var matching = session.$findOpeningBracket(')', {column: cursor.column + 1, row: cursor.row});
-                        if (matching !== null && CstyleBehaviour.isAutoInsertedClosing(cursor, line, text)) {
-                            CstyleBehaviour.popAutoInsertedClosing();
-                            return {
-                                text: '',
-                                selection: [1, 1]
-                            };
-                        }
-                    }
-                }
-            });
-    
+            this.inherit(CstyleBehaviour, ["braces", "parens", "string_dquotes"]); // Get string behaviour
+            this.parent = parent;
         }
+    
         oop.inherits(XQueryBehaviour, Behaviour);
     
         exports.XQueryBehaviour = XQueryBehaviour;
@@ -529,7 +444,6 @@ define("eXide/mode/xquery_highlight_rules", function(require, exports, module) {
         }
         
         var XMLBehaviour = function (parent) {
-            
             this.inherit(CstyleBehaviour, ["braces", "parens", "string_dquotes"]); // Get string behaviour
             this.parent = parent;
             
